fb32a095ed4d8c4f2ec1cd3c39b18a5e5f7b4622
m <m@example.org>
2018-12-22 09:36:13 +0000

    refixed license & documentation

--- lib/SubSystem/CachedDB.pm
+++ lib/SubSystem/CachedDB.pm
@@ -98,53 +98,12 @@ sub debug {
 
 mmacnair, C<< <mmacnair at cpan.org> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-subsystem-cacheddb at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=SubSystem-CachedDB>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc SubSystem::CachedDB
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=SubSystem-CachedDB>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/SubSystem-CachedDB>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/SubSystem-CachedDB>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/SubSystem-CachedDB>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2018 mmacnair.
 
-This program is distributed under the (Simplified) BSD License:
-L<http://www.opensource.org/licenses/BSD-2-Clause>
+This program is distributed under the (Revised) BSD License:
+L<http://www.opensource.org/licenses/BSD-3-Clause>
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -156,6 +115,11 @@ notice, this list of conditions and the following disclaimer.
 * Redistributions in binary form must reproduce the above copyright
 notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
+
+* Neither the name of mmacnair's Organization
+nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written
+permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
